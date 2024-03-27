de8436ca8df9bb481e37618e6f77e09145e49806
root <lseirina87@gmail.com>
2024-03-27 11:59:32 +0300

    Added fixed cats.py

--- app/cats.py
+++ app/cats.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+API_CATS_URL = 'https://randomfox.ca/floof/'
 BOT_TOKEN = '6883498485:AAGtOZFurG3T-H2oDNwhQcUqeUzlMfqcJHE'
 ERROR_TEXT = 'There should be image with cats:('
 
@@ -14,17 +14,17 @@ cat_link: str
 res = requests.get(API_CATS_URL)
 while counter < 100:
     print('attempt =', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates').json()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
             cat_response = requests.get(API_CATS_URL)
             if cat_response.status_code == 200:
-                photo = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&photo={photo}')
+                photo = cat_response.json()['image']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={photo}')
             else:
-                print(ERROR_TEXT)
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
