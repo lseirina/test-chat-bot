8d288b231a87f1e769498a190e0891aba98a6981
root <lseirina87@gmail.com>
2024-04-04 21:25:48 +0300

    Added logging

---
+++ exm_logging.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug("This for debug.")
+logging.info("This for info.")
+logging.warning("This is for warning")
+logging.error("This is log of error level")
+logging.critical("This is log of critical lavel")
