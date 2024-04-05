1f4974cdb4ce44752ee7857ae0e423650f88a529
root <lseirina87@gmail.com>
2024-04-05 13:27:31 +0300

    Added modifided logging

--- exm_logging.py
+++ exm_logging.py
@@ -1,9 +1,24 @@
 import logging
 
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] #%(levelname)-8s %(filename)s'
+           '%(lineno)s - %(name)s - %(message)s'
+)
 
-logging.debug("This for debug.")
-logging.info("This for info.")
-logging.warning("This is for warning")
-logging.error("This is log of error level")
-logging.critical("This is log of critical lavel")
+logger = logging.getLogger(__name__)
+
+logger.debug('Log of the level DEBUG')
+
+
+# logging.basicConfig(level=logging.CRITICAL)
+
+# logger = logging.getLogger(__name__)
+
+# logging.debug("This for debug.")
+# logging.info("This for info.")
+# logging.warning("This is for warning")
+# logging.error("This is log of error level")
+# logging.critical("This is log of critical lavel")
+
+# print(dir(logger))
