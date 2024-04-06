63ab03605dd3df7b89606bcf55d8fa1f47990690
root <lseirina87@gmail.com>
2024-04-06 21:43:44 +0300

    Added try_except logging

---
+++ loging/logg_try_except.py
@@ -0,0 +1,11 @@
+"""выводить логи уровня ERROR, но с полным трейсбэком ошибки"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    print(4 / 2)
+    print(2 / 0)
+except ZeroDivisionError:
+    # logger.error('There is exception here.', exc_info=True) # the first option.
+    logger.exception('There is an exception here.') # The second option.
